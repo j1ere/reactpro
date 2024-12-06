04b39a1bcd734dad0c4cdb3dcb9c326c70b10316
j1ere <jeremywambua106@gmail.com>
2024-12-06 09:05:25 +0300

    conditional components example with fruit list

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -3,7 +3,7 @@ import Welcome from "./Welcome";
 
 export default function ConditionalComponent() {
   let message;
-  const display = true;
+  const display = false;
 
   return display ? <h1>This is message 1 t</h1> : <h1>This is message 2</h1>;
 }
--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -1,5 +1,8 @@
 export default function Fruit(props) {
-  return <div>
-    {props.name} and {props.price}
-  </div>;
+  return (
+    <div>
+      {/* {props.name} and {props.price} */}
+      {props.price > 10 ? <h1>{props.name} and {props.price}</h1> : null}
+    </div>
+  );
 }
