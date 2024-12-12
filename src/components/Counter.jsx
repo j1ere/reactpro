8b666c8203cd712d30448356e71a7fe4ccf5db48
j1ere <jeremywambua106@gmail.com>
2024-12-12 13:22:21 +0300

    states part 2

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,16 +9,31 @@ export default function Counter() {
   // through the help of setCount
   const [count, setCount] = useState(0);
   function handleIncrement() {
-    setCount(count + 1);
+    setCount(count + value);
   }
   function handleDecrement() {
-    setCount(count - 1);
+    setCount(count - value);
+  }
+
+  const [value, incrementValue] = useState(1);
+
+  function increaseIncrementValue() {
+    incrementValue(value + 1);
+  }
+
+  function decreaseIncrementValue() {
+    incrementValue(value - 1);
   }
   return (
     <div>
       <h3>Count value is: {count}</h3>
       <button onClick={handleIncrement}>increment</button>{" "}
       <button onClick={handleDecrement}>decrement</button>
+      <br />
+      <br />
+      <h2>we are inceamenting the value by:{value}</h2>
+      <button onClick={increaseIncrementValue}>Increase Value</button>{" "}
+      <button onClick={decreaseIncrementValue}>decrease value</button>
     </div>
   );
 }
